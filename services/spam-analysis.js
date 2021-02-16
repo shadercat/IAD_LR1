c7c155909d8bd9c977943bfab5903f08eb1b2611
Thang Tran <haswell777@live.com>
2021-02-16 20:11:41 +0200

    commit infrastructure

---
+++ services/spam-analysis.js
@@ -0,0 +1,45 @@
+// https://github.com/shiffman/bayes-classifier-js
+
+class Sentence {
+    constructor(string, isSpam) { //string, boolean
+        this.string = string;
+        this.isSpam = isSpam;
+    }
+}
+
+class GuessingStats {
+    constructor(percent, quantityOfSentences) { // percent is int from 0 to 100, quantityOfSentences is int from 1 to ...
+
+    }
+}
+
+function learnExamples(sentences) { // Sentence[]
+    
+} // no return
+
+function isSpam(string) { //string
+
+} // returns boolean
+
+function getStatistics(sentences) { // Sentence[]
+
+} // returns GuessingStats[]
+
+function thisIsAnExampleAndItWontBeUsed() {
+    let classifier = new Classifier();
+
+    // Text to train, followed by category name
+    classifier.train("New meeting tomorrow (file)", "+");
+    classifier.train("Corporate party tomorrow", "+");
+    classifier.train("New greeting text", "+");
+
+    classifier.train("Free sales party", "spam");
+    classifier.train("Free file for you", "spam");
+    classifier.train("Free file upload", "spam");
+
+    classifier.probabilities();
+
+    let results = classifier.guess("Free file tomorrow");
+
+    return classifier.guess("Free file tomorrow")['+'].probability;
+}
